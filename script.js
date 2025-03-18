8bf1780f020bd5a856099326e8cab343105fdb5a
Juris Raudulis <juris.raudulis@outlook.com>
2025-03-18 15:07:51 +0000

    Uploading first files

---
+++ script.js
@@ -0,0 +1,64 @@
+// 1 on buttom click fetch data from api with specific  from input
+// 2 display data on display
+
+// dom selectors
+const searchInput = document.getElementById('search');
+const btn = document.getElementById('btn');
+const locationName = document.getElementById('location');
+const temperature = document.getElementById('temperature');
+const weatherInfoContainer = document.querySelector('.weather-info');
+const loadAnimation = document.querySelector('.loader');
+
+
+async function fetchLocationCoordinates() {
+    let input = searchInput.value;
+
+    if (!input) {
+        return alert('Enter location');
+    } else {
+        try {
+            weatherInfoContainer.style.display = 'none';
+            loadAnimation.style.display = 'block';
+            let data = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=08ff5bfd6bbd0c08f59cd1c0c38d242b`);
+                let resp = await data.json();
+                if (!resp || resp.length === 0) {
+                    loadAnimation.style.display = 'none';
+                    weatherInfoContainer.style.display = 'block';
+                    return alert('Enter valid locaton');
+                } else {
+                    const location = resp[0].name;
+                    const latitude = resp[0].lat;
+                    const longitude = resp[0].lon;
+                    fetchTemperatureData(latitude, longitude, location);
+                }  
+            } catch(err) {
+                loadAnimation.style.display = 'none';
+                alert(`Error occured while getting data: ${err.message}`);
+            }
+    }
+}
+
+async function fetchTemperatureData(latitude, longitude, location){
+    try{
+     let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=08ff5bfd6bbd0c08f59cd1c0c38d242b&units=metric`);
+    let resp = await data.json();
+
+    if (!resp.main) return alert('Server error');
+    else {
+        let temperatureData = Math.round(resp.main.temp);
+        loadAnimation.style.display = 'none';
+        weatherInfoContainer.style.display = 'block';
+        locationName.textContent = `${location}`;
+        temperature.textContent = `${temperatureData}ºC`;
+        searchInput.value = '';  
+    }
+     } catch(err) {
+            alert(`Error while recieving temperature data: ${err.message}`);
+        } finally {
+            loadAnimation.style.display = 'none';
+            weatherInfoContainer.style.display = 'block';
+        }
+}
+
+
+btn.addEventListener('click', fetchLocationCoordinates);
